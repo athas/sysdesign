25624bbc5fd0e380447a8862ceec7ee16cd135c7
Troels Thrane <troels@x200s.(none)>
2010-01-18 11:34:40 +0100

    Updating cleaner..

--- assignment2/implementation/cleaner.py
+++ assignment2/implementation/cleaner.py
@@ -2,89 +2,97 @@ import os
 import time
 from datetime import datetime, timedelta
 from time import mktime
+from locking import *
 
 FILENAME_LOCKED   = 'lock'   # filename used when a file is locked
 FILENAME_UNLOCKED = 'unlock' # filename used when a file is unlocked
 RENAME_TIMEOUT    = 2        # time to wait between each retry to lock file
 FILE_SUFFIX       = ';0'     # suffix for a file's revision-directory
 DIR_SUFFIX        = ';*'     # suffix for a directory's revision-directory
-LANDMARK_TIME     = 3600     # If revisions are within this boundary, they should be merged
-TEST              = True     # Enables testmode
+TEST              = False    # Enables testmode
+TEST_DATA = {
+    'file1.txt;0' :
+        [['file1.txt;1', 1213270794.0],
+         ['file1.txt;2', 1213271798.0],
+         ['file1.txt;3', mktime((datetime.now()+timedelta(days=-31)).timetuple())],
+         ['file1.txt;4', mktime((datetime.now()+timedelta(days=-29)).timetuple())],
+         ['file1.txt;5', mktime((datetime.now()+timedelta(days=-1)).timetuple())]],
+    'file2.txt;0' :
+        [['file2.txt;1', 1213270794.0],
+         ['file2.txt;2', 1213271798.0],
+         ['file2.txt;3', 1213271898.0],
+         ['file2.txt;4', 1213271998.0],
+         ['file2.txt;5', mktime((datetime.now()+timedelta(days=-1)).timetuple())]]
+    }
 
 
-# Creates a list of all the files and directories.
-def list_dir (rootdir):
-    files = list()
-    dirs = list()
-    entries = os.listdir(rootdir)
-    for entry in entries:
-        if entry.endswith(FILE_SUFFIX):
-            files.append(entry)
-        elif entry.endswith(DIR_SUFFIX):
-            dirs.append(entry)
-    os.chdir(rootdir)
-    return files,dirs
+class Cleaner:
 
-# Creates a list containing all file-revisions and their timestamp.
-def list_revisions (rootdir ,file):
-    revisions = list()
-    os.chdir(file)
-    for file in os.listdir('.'):
-           if file not in [FILENAME_LOCKED, FILENAME_UNLOCKED]:
-               time = os.path.getctime(file) # should be good enough, as files should never be changed after a newer revision is available
-               revisions.append([file,time])
+    def __init__(self, dir, landmark_time, landmark_limit):
+       self.landmark_time = landmark_time
+       self.landmark_limit = landmark_limit
+       self.rootdir = dir
+       self.cleaner()
 
-    os.chdir(rootdir)
-    return sorted(revisions)
-
-# Locks a file
-def lock_file (rootdir, file):
-    # Change dir to file-revision-dir
-    os.chdir(file)
-    for t in range(1,4):
-        try: 
-            os.rename(FILENAME_UNLOCKED, FILENAME_LOCKED)
-            break
-        except:
-            print 'Locking file failed ' + str(t) + ' times'
-            time.sleep(RENAME_TIMEOUT)
-    os.chdir(rootdir)
-
-def unlock_file (rootdir, file):
-    os.chdir(file)
-    try:
-        os.rename(FILENAME_LOCKED, FILENAME_UNLOCKED)
-    except:
-        print 'Failed in unlocking file'
-    os.chdir(rootdir)
-
-def clean_file (rootdir, file):
-    revisions = list_revisions(rootdir, file)
-    last_timestamp = 0
-    last_filename = ''
-    
-    t = datetime.now()
-    last_month = mktime((t+timedelta(days=-30)).timetuple())
-
-    for rev,time in revisions:
-        if time > last_month:
-            # If the revision is never than one month, let the rest be.
-            break;
+    # Creates a list of all the files and directories.
+    def list_dir (self):
+        files = list()
+        dirs = list()
+        entries = list()
+        if TEST:
+            entries = TEST_DATA.keys()
         else:
-            if time - LANDMARK_TIME < last_timestamp:
-                # Remove old revision
-                os.remove(last_filename)
-        last_filename = rev
-        last_timestamp = time
-    
-    print revisions
+            entries = os.listdir(self.rootdir)
 
-def cleaner (rootdir):
-    os.chdir(rootdir)
-    files, dirs = list_dir(rootdir)
-    for file in files:
-        clean_file(rootdir, file)
-    for dir in dirs:
-        cleaner(rootdir+dir)
+        for entry in entries:
+            if entry.endswith(FILE_SUFFIX):
+                files.append(self.rootdir + entry)
+            elif entry.endswith(DIR_SUFFIX):
+                dirs.append(self.rootdir + entry)
+        return files,dirs
 
-cleaner('/home/troels/test/')
+    # Creates a list containing all file-revisions and their timestamp.
+    def list_revisions (self, file):
+        revisions = list()
+        if TEST:
+            return TEST_DATA[file]
+
+        for rev in os.listdir(file):
+            if rev not in [FILENAME_LOCKED, FILENAME_UNLOCKED]:
+                time = os.path.getctime(file) # should be good enough, as files should never be changed after a newer revision is available
+                revisions.append([self.rootdir + file + rev,time])
+
+        return sorted(revisions)
+
+    def clean_file (self, file):
+        revisions = self.list_revisions(file)
+        last_timestamp = 0
+        last_filename = ''
+        
+        # The time it moves from KeepAll- to Landmark-retention
+        landmark_limit = mktime((datetime.now()+timedelta(days=-30)).timetuple())
+
+        for rev,time in revisions:
+            if time > landmark_limit:
+                # If the revision is never than one month, let the rest be.
+                break;
+            else:
+                if time - self.landmark_time < last_timestamp:
+                    # Remove old revision
+                    if TEST:
+                        print 'Removing ' + last_filename
+                    else:
+                        os.remove(last_filename)
+            last_filename = rev
+            last_timestamp = time
+
+    def cleaner (self):
+        files, dirs = self.list_dir()
+        for file in files:
+            if lock_file(file):
+                self.clean_file(file)
+                unlock_file(file)
+        for dir in dirs:
+            Cleaner(rootdir+dir, self.landmark_time, self.landmark_limit)
+
+cleaner = Cleaner('/home/troels/test/', 3600, 31)
