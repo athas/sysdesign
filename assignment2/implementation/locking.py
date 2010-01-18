bb12716a30b61568c0fe65cb8f4d176b7eca1967
Troels Henriksen <athas@lambda.athas.dyndns.dk>
2010-01-18 10:52:22 +0100

    Added locking.py.

---
+++ assignment2/implementation/locking.py
@@ -0,0 +1,21 @@
+import os
+
+def lock_file (rootdir, file):
+    # Change dir to file-revision-dir
+    os.chdir(file)
+    for t in range(1,4):
+        try: 
+            os.rename(FILENAME_UNLOCKED, FILENAME_LOCKED)
+            break
+        except:
+            print 'Locking file failed ' + str(t) + ' times'
+            time.sleep(RENAME_TIMEOUT)
+    os.chdir(rootdir)
+
+def unlock_file (rootdir, file):
+    os.chdir(file)
+    try:
+        os.rename(FILENAME_LOCKED, FILENAME_UNLOCKED)
+    except:
+        print 'Failed in unlocking file'
+    os.chdir(rootdir)
